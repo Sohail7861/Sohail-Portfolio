c6d04032fadce43d695a92847f99967f1668d2b7
Sohail Mohammad <92430476+Sohail7861@users.noreply.github.com>
2024-04-17 20:30:26 -1200

    Update Frontend.jsx

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -11,14 +11,14 @@ const Frontend = () => {
                         <HiCheckBadge />
                         <div>
                             <h3 className="skills__name">HTML</h3>
-                            <span className="skills__level">Intermediate</span>
+                            <span className="skills__level">Advanced</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
                             <h3 className="skills__name">CSS/SASS</h3>
-                            <span className="skills__level">Intermediate</span>
+                            <span className="skills__level">Advanced</span>
                         </div>
                     </div>
                     <div className="skills__data">
@@ -34,21 +34,21 @@ const Frontend = () => {
                         <HiCheckBadge />
                         <div>
                             <h3 className="skills__name">JavaScript</h3>
-                            <span className="skills__level">Intermediate</span>
+                            <span className="skills__level">Advanced</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
                             <h3 className="skills__name">React</h3>
-                            <span className="skills__level">Intermediate</span>
+                            <span className="skills__level">Advanced</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <HiCheckBadge />
                         <div>
                             <h3 className="skills__name">Redux</h3>
-                            <span className="skills__level">Basic</span>
+                            <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
                 </div>
